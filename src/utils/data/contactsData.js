c269925eaa1d0740e0f2fd22e49b662e2186373c
Himanshu <112165555+HimanshuXD0@users.noreply.github.com>
2025-05-13 18:37:35 +0530

    Update contactsData.js

--- src/utils/data/contactsData.js
+++ src/utils/data/contactsData.js
@@ -2,11 +2,11 @@ export const contactsData = {
     name:'Himanshu Sharma',
     email: 'sharmahimanshu2429@gmail.com',
     phone: '+917389732429',
-    address: 'Gurgaon, Haryana',
+    address: 'Noida ,Uttar Pradesh',
     github: 'https://github.com/HimanshuXD0',
     linkedIn: 'https://www.linkedin.com/in/himanshuxd0/',
     leetcode:'https://www.geeksforgeeks.org/user/himanshu2924/',
     twitter:'https://links.cuvette.tech/student/66a3372ebc739b0f90535179',
-    resume: 'https://drive.google.com/file/d/14H40z-_F4Hx5O-mDlArWcdsqzj385iY9/view?usp=drive_link',
+    resume: 'https://drive.google.com/file/d/1ASKCBB-83YjTBMmhEQ3TOdL9ipgn92D9/view?usp=sharing',
     aboutMe:'An enthusiastic engineer with a solid grasp of coding and a passion for building innovative solutions. Driven by real-world challenges, I thrive in inspirational environments and aim to use my skills to contribute to meaningful projects and advancements.'
 }
